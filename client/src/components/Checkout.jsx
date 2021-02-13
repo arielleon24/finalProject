7fdef589ff23ed240416cb64252a120eed674342
Raphaelle Paquet <68092222+raphpaquet@users.noreply.github.com>
2021-02-13 16:09:28 -0500

    rm Stripe Key

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -3,12 +3,14 @@ import { StripeProvider, Elements } from 'react-stripe-elements'
 import CheckoutForm from './CheckoutForm'
 
 const Checkout = ({ selectedProduct, history, user }) => {
+const REACT_STRIPE_API_KEY=process.env.REACT_STRIPE_API_KEY;
+
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
 
   return (
-    <StripeProvider apiKey="pk_test_51I6Jt9HsmgbtTrbqfCLFE3iOym9EzX83fUD26QzcT23sPMbAdp0lCcj0gev2ldujHpqI7HbTMa3unA5acP6UaN3U00AcA7HfMG">
+    <StripeProvider apiKey={REACT_STRIPE_API_KEY}>
       <Elements>
         <CheckoutForm user = {user} selectedProduct={selectedProduct} history={history} />
       </Elements>
